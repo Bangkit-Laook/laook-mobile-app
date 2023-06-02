337d1630a2e766d848c8b4440b0bb1ef66170074
Dhafa <dhofaaryanda4@gmail.com>
2023-06-02 16:59:50 +0700

    add scan

--- app/src/main/java/com/example/laook/ScanActivity.kt
+++ app/src/main/java/com/example/laook/ScanActivity.kt
@@ -1,17 +1,44 @@
 package com.example.laook
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import com.google.android.material.appbar.MaterialToolbar
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import com.example.laook.databinding.ActivityScanBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class ScanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScanBinding
+    private lateinit var currentPhotoPath: String
+    private var getFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan)
+
+        binding = ActivityScanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Find the back button ImageView
         val btnBack: ImageView = findViewById(R.id.btnBack)
@@ -26,7 +53,132 @@ class ScanActivity : AppCompatActivity() {
         btnInfo.setOnClickListener {
             showInfoDialog()
         }
+
+        binding.btnCamera.setOnClickListener {
+            requestCameraPermission()
+        }
+
+        binding.btnGallery.setOnClickListener {
+            startGallery()
+        }
+
+//        binding.btnAccept.setOnClickListener {
+//            uploadImage()
+//        }
+
     }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            myFile.let { file ->
+                getFile = file
+                binding.ivPhoto.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@ScanActivity,
+                getString(R.string.package_name),
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@ScanActivity)
+                getFile = myFile
+                binding.ivPhoto.setImageURI(uri)
+            }
+        }
+    }
+
+    private fun requestCameraPermission() {
+        val cameraPermission = Manifest.permission.CAMERA
+        val hasCameraPermission = ContextCompat.checkSelfPermission(
+            this,
+            cameraPermission
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (hasCameraPermission) {
+            startTakePhoto()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(cameraPermission),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startTakePhoto()
+            } else {
+                // Izin kamera ditolak, berikan tindakan alternatif atau beri tahu pengguna
+                // tentang kebutuhan izin tersebut
+            }
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, getString(R.string.choose_picture))
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun uriToFile(selectedImg: Uri, context: Context): File {
+        val contentResolver: ContentResolver = context.contentResolver
+        val myFile = createCustomTempFile(context)
+
+        val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+        val outputStream: OutputStream = FileOutputStream(myFile)
+        val buf = ByteArray(1024)
+        var len: Int
+        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+        outputStream.close()
+        inputStream.close()
+
+        return myFile
+    }
+
+
+    private fun createCustomTempFile(context: Context): File {
+        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(timeStamp, ".jpg", storageDir)
+    }
+
+    private val timeStamp: String = SimpleDateFormat(
+        FILENAME_FORMAT,
+        Locale.US
+    ).format(System.currentTimeMillis())
 
     private fun showInfoDialog() {
         val builder = AlertDialog.Builder(this)
@@ -46,5 +198,10 @@ class ScanActivity : AppCompatActivity() {
         }
 
         dialog.show()
+    }
+
+    companion object {
+        const val FILENAME_FORMAT = "dd-MMM-yyyy"
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
     }
 }
