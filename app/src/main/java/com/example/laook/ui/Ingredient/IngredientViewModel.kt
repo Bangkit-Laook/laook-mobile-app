c650fb7f7649672c8bfb36d09321305d41794bc6
Dhafa <dhofaaryanda4@gmail.com>
2023-06-15 21:55:56 +0700

    update ingredient viewmodel

--- app/src/main/java/com/example/laook/ui/Ingredient/IngredientViewModel.kt
+++ app/src/main/java/com/example/laook/ui/Ingredient/IngredientViewModel.kt
@@ -16,7 +16,7 @@ class IngredientViewModel : ViewModel() {
 
     fun getAllIngredients(): List<String> {
         // All ingredient
-        val allIngredients = listOf("ing_1", "ing_2", "ing_3", "ing_4", "ing_5","ing_6","ing_7","ing_8","ing_9","ing_10","ing_11")
+        val allIngredients = listOf("nasi", "ayam", "telur", "daging", "bebek", "ikan", "udang","cumi","kepiting","wortel","tempe","tahu","kentang", "bawang", "jagung", "kol", "cengkeh")
 
         return allIngredients
     }
